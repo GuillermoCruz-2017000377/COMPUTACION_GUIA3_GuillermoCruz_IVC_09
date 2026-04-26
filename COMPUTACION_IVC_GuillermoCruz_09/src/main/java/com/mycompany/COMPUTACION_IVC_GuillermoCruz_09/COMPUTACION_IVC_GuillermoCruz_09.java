f63e39cfa2e640c6c95e3e9e11fbcc4e7c705cdc
GuillermoCruz-2017000377 <j.cruzg0904@gmail.com>
2026-04-25 18:35:08 -0600

    Add files via upload

---
+++ COMPUTACION_IVC_GuillermoCruz_09/src/main/java/com/mycompany/COMPUTACION_IVC_GuillermoCruz_09/COMPUTACION_IVC_GuillermoCruz_09.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.COMPUTACION_IVC_GuillermoCruz_09;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class COMPUTACION_IVC_GuillermoCruz_09 {
+
+    public static void main(String[] args) {
+        jfrmMensajes Yoni=new jfrmMensajes();
+        Yoni.setVisible(true);
+    }
+}
